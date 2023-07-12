6050d69db9120b123c7a19410ff01c07b642f5c8
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-12 11:20:23 +0530

    client- src- components- Layout- Layout.js- Change toggle icon code processing...

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineDoubleLeft } from "react-icons/ai";
 import "./layout.css";
 import Home from "../../pages/Home/Home";
 
 const Layout = () => {
+  const [toggle, setToggle] = useState(true);
+
+  //Change Toggle icon
+  
   return (
     <>
       <div className="sidebar-section">
