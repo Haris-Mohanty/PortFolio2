cd42ba33a52d548e7b8cb23cf2033095d110f328
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-13 10:31:44 +0530

    client- src- pages- Home- Home.js- Homepage design processing..

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
       <div className="container-fluid home-container">
-        <div className="container">
+        <div className="container home-content">
           <h1>Hi ! I'm a</h1>
         </div>
       </div>
