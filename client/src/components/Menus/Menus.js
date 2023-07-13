35ce08a89dd22798f255e47f8034f64aa47949d0
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-13 10:15:10 +0530

    client- src- components- Menus- Menus.js- Add sidenav menu code fixed.

--- client/src/components/Menus/Menus.js
+++ client/src/components/Menus/Menus.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Menus.css";
 import profilePic from "./../../Assets/Images/profile-pic.png";
-import { FcHome } from "react-icons/fc";
+import {
+  FcHome,
+  FcAbout,
+  FcPortraitMode,
+  FcBiotech,
+  FcReadingEbook,
+  FcVideoProjector,
+  FcVoicePresentation,
+  FcBusinessContact,
+} from "react-icons/fc";
 
 const Menus = ({ toggle }) => {
   return (
@@ -17,11 +26,68 @@ const Menus = ({ toggle }) => {
                 <FcHome />
                 Home
               </div>
+              <div className="nav-link">
+                <FcAbout />
+                About
+              </div>
+              <div className="nav-link">
+                <FcPortraitMode />
+                Experience
+              </div>
+              <div className="nav-link">
+                <FcBiotech />
+                Tech Stack
+              </div>
+              <div className="nav-link">
+                <FcReadingEbook />
+                Education
+              </div>
+              <div className="nav-link">
+                <FcVideoProjector />
+                Projects
+              </div>
+              <div className="nav-link">
+                <FcVoicePresentation />
+                Testimonial
+              </div>
+              <div className="nav-link">
+                <FcBusinessContact />
+                Contact
+              </div>
             </div>
           </div>
         </>
       ) : (
-        <h1>Icons</h1>
+        <>
+          <div className="nav-items">
+            <div className="nav-item navbar-item">
+              <div className="nav-link">
+                <FcHome title="Home" />
+              </div>
+              <div className="nav-link">
+                <FcAbout title="About" />
+              </div>
+              <div className="nav-link">
+                <FcPortraitMode title="Work Experience" />
+              </div>
+              <div className="nav-link">
+                <FcBiotech title="Tech Stack" />
+              </div>
+              <div className="nav-link">
+                <FcReadingEbook title="Eduction" />
+              </div>
+              <div className="nav-link">
+                <FcVideoProjector title="Projects" />
+              </div>
+              <div className="nav-link">
+                <FcVoicePresentation title="Testimonial" />
+              </div>
+              <div className="nav-link">
+                <FcBusinessContact title="Contact" />
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
