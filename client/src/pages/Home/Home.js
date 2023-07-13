48c27eab76c8086e4c71d0710453397c97cc9c82
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-13 10:52:06 +0530

    client- src- pages- Home- Home.js- Typewriter added successfully.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
@@ -7,6 +8,20 @@ const Home = () => {
       <div className="container-fluid home-container">
         <div className="container home-content">
           <h1>Hi ! I'm a</h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  "Mern-stack Developer!",
+                  "Coder!",
+                  "Problem Solver!",
+                  "Full-stack developer!",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
         </div>
       </div>
     </>
