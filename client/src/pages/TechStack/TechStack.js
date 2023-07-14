407b97fece0213d40b14c3156e1ff1bda3b20dcb
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-14 09:40:50 +0530

    client- src- pages- TechStack- TechStack.js- Techstack list fetched successfully.

--- client/src/pages/TechStack/TechStack.js
+++ client/src/pages/TechStack/TechStack.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./TechStack.css";
+import { TechstackList } from "../../utils/TechStackList";
 
 const TechStack = () => {
   return (
     <>
       <div className="container techstack">
-        <h2 className="col-12 mt-4 mb-1 text-center font-Lato">Technologies Stack</h2>
+        <h2 className="col-12 mt-4 mb-1 text-center font-Lato">
+          Technologies Stack
+        </h2>
         <hr />
         <p className="pb-3 text-center">
           ➡️Including Programming Languages, Frameworks, Databases, Front-end,
           Back-end tools, APIs!
         </p>
+        <div className="row">
+          {TechstackList.map((tech) => (
+            <h1>{tech.name}</h1>
+          ))}
+        </div>
       </div>
     </>
   );
