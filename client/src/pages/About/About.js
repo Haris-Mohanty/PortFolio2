a04ca1d4ac16618027a74684a30069c2aeb7b80a
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-14 08:41:36 +0530

    client- src- pages- About- About.js- About us code processing....

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./about.css"
+
+const About = () => {
+  return (
+    <>
+      <div className='about'>
+        <div className='row'>
+            <div className='col-md-6'></div>
+            <div className='col-md-6'></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default About
