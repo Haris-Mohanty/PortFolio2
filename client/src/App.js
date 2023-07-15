8aaf53fb923fae24ff84f93a5494962958c49d13
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-15 11:25:23 +0530

    client- src- pages- Projects- Projects.js- code processing....

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Layout from "./components/Layout/Layout";
 import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
 import TechStack from "./pages/TechStack/TechStack";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="container">
         <About />
         <TechStack />
+        <Projects/>
       </div>
     </>
   );
--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Projects.css";
 
 const Projects = () => {
   return (
     <div>
-      
+      <div className="container project">
+        <h1>Projects</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
