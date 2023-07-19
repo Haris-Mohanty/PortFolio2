7e5b2ad9fa2f1091f00dca1492c8b2b6ef0cdad4
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-19 12:05:31 +0530

    client- src- pages- Projects- Projects.js- Show projects code processing......

--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./Projects.css";
+import projectImg1 from "./../../Assets/Images/project-img1.png";
+import projectImg2 from "./../../Assets/Images/project-img3.png";
+import projectImg3 from "./../../Assets/Images/project-img4.png";
+import projectImg4 from "./../../Assets/Images/project-img5.png";
+import projectImg5 from "./../../Assets/Images/project-img6.png";
+import projectImg6 from "./../../Assets/Images/project-img2.png";
 
 const Projects = () => {
   return (
@@ -14,7 +20,29 @@ const Projects = () => {
         </p>
         <div className="row" id="ads">
           <div className="col-md-4">
-            
+            <div className="card rounder">
+              <div className="card-image">
+                <span className="card-notify-badge">Front End</span>
+                <img src={projectImg1} alt="Project1" className="w-100" />
+              </div>
+              <div className="card-image-overly m-auto py-2">
+                <span className="card-detail-badge px-2">HTML</span>
+                <span className="card-detail-badge px-2">CSS</span>
+                <span className="card-detail-badge px-2">JS</span>
+                <span className="card-detail-badge px-2">Bootstrap</span>
+              </div>
+              <div className="card-body">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase">Education App Development</h5>
+                </div>
+                <a
+                  href="https://github.com/Haris-Mohanty/Education-App-Development"
+                  className="ad-btn"
+                >
+                  View
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
