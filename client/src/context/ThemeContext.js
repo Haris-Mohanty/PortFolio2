f1a2dd80ef533d470bef57315e3a243461153cd4
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-25 09:39:43 +0530

    client- src- ThemeContext.js- Context added successfully.

--- client/src/context/ThemeContext.js
+++ client/src/context/ThemeContext.js
@@ -5,5 +5,9 @@ const ThemeContext = createContext();
 const ThemeProvider = ({ children }) => {
   const { theme, setTheme } = useState("light");
 
-  
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
