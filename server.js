bc9b967b4ff16c85e16fab019bca0e0170aad25a
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-27 12:08:14 +0530

    server.js- Code Added.

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
+// dotenv Configuration
+dotenv.config();
+
+//Rest Object
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Success");
+});
+
+//Port
+const port = process.env.PORT || 8080;
+
+//Listen
+app.listen(port, () => {
+  console.log(`Server Running on PORT ${port}`);
+});
