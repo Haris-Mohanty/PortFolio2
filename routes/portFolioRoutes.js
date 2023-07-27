f80b6f2091a5b817409d32b72535cd994021010d
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-27 12:12:34 +0530

    routes- portFolioRoutes.js- Routes created successfully.

--- routes/portFolioRoutes.js
+++ routes/portFolioRoutes.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const { sendEmailController } = require("../controller/portFolioController");
+
+//Routes Object
+const router = express.Router();
+
+//Routes
+router.post("/sendMail", sendEmailController);
+
+//EXport
+module.exports = router;
